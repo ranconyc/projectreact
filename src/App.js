f98e5e0fbbe72cb13d4d8bfe923974a7e5165ea6
ran cohen <ranco.nyc@gmail.com>
2020-09-01 11:05:56 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Tweet from './tweet';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([
+    {name: "Ran", message: "I like pizza", likes: "10" },
+    {name: "Ben", message: "I like to sleep", likes: "20" },
+    {name: "Lee", message: "I like to dance", likes: "40" }
+  ]);
+
+  const Increment = () => {
+    setCount(count + 1);
+  }
+  
+  const changeColor = () => {
+    setRed(!isRed);
+  }
+
+  return(
+    <div className="app">
+      <h2 className={isRed ? "red" : ""}>Change my color!</h2>
+      <button onClick={ changeColor }>Change color</button>
+      <h1>{count}</h1>
+      <button onClick={ Increment }>Increment</button>
+      {users.map(u => (
+        <Tweet name={u.name} message={u.message} likes={u.likes} />
+      ))}
     </div>
   );
 }
